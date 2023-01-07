b70cb585d7910cf501a58c6f6225660adb602351
Raman-79 <ramandsat@gmail.com>
2023-01-07 11:17:38 +0530

    Tables showing from backend to the frontend

--- app.js
+++ app.js
@@ -3,23 +3,26 @@ const app = express();
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const path = require('path');
+const { error } = require('console');
 
 const mysql = require('./sqlConnection').con;
 dotenv.config();
 const port = process.env.PORT;
+let student = [];
+app.set('view engine', 'ejs');
 
-app.set('view engine', 'hbs');
-
-
-app.set('views', path.join(__dirname, "./views"));
+app.set('views', path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.render('signup');
+    res.render('home');
 });
-app.post('/', (req, res) => {
+app.get('/signup', (req, res) => {
+    res.render('signup');
 
+});
+app.post('/signup', (req, res) => {
     var { name, usn, cgpa, branch_code, batch, password } = req.body;
     var query1 = "insert into student (USN,CGPA,Branch_code,Name,Password,Batch)  values ('" + usn + "','" + cgpa + "','" + branch_code + "','" + name + "','" + password + "','" + batch + "')";
     mysql.query(query1, function (error, result) {
@@ -28,10 +31,7 @@ app.post('/', (req, res) => {
     });
 });
 app.get('/login', (req, res) => {
-    res.render('login', {
-        message,
-        msg2
-    });
+    res.render('login');
 })
 app.get('/cmpLogin', (req, res) => {
     res.render('cmpLogin');
@@ -42,4 +42,12 @@ app.get('/cmpSignUp', (req, res) => {
 app.listen(port, () => {
     console.log(`Server connection successfull at ${port}`);
 });
+app.get('/studentDets', (req, res) => {
+    var query = "select * from student";
+    mysql.query(query, (error, result) => {
+        if (error) throw error;
+        student = result;
+        res.render("studentDets", { student });
+    });
+})
 
