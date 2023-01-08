a66082632bafc6409f3ae5127403702bddf5c5b1
Raman-79 <ramandsat@gmail.com>
2023-01-08 05:48:29 +0530

    Admin Authentication

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { error } = require('console');
 const mysql = require('./sqlConnection').con;
 dotenv.config();
 const port = process.env.PORT;
-let student = [];
+
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, "views"));
@@ -16,18 +16,37 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { success: null });
 });
 app.get('/signup', (req, res) => {
     res.render('signup');
-
 });
+app.get('/adminHome', (req, res) => {
+    const admin = req.query.admin;
+    const password = req.query.password;
+    if (admin === "admin" && password === "admin@123") {
+        var query = "select * from student";
+        mysql.query(query, (error, result) => {
+            if (error) throw error;
+            res.render("adminHome", { result, success: true });
+        });
+    }
+    else {
+        res.render('home', { success: false });
+    }
+})
 app.post('/signup', (req, res) => {
     var { name, usn, cgpa, branch_code, batch, password } = req.body;
     var query1 = "insert into student (USN,CGPA,Branch_code,Name,Password,Batch)  values ('" + usn + "','" + cgpa + "','" + branch_code + "','" + name + "','" + password + "','" + batch + "')";
     mysql.query(query1, function (error, result) {
         if (error) throw error;
-        res.send("Entry Succesfull");
+        else {
+            var query2 = "select Name,USN,CGPA,Branch_code,Batch from student where USN = ? ";
+            mysql.query(query2, usn, (error, result) => {
+                if (error) throw error;
+                res.render('studentHome', { result });
+            });
+        }
     });
 });
 app.get('/login', (req, res) => {
@@ -42,12 +61,11 @@ app.get('/cmpSignUp', (req, res) => {
 app.listen(port, () => {
     console.log(`Server connection successfull at ${port}`);
 });
-app.get('/studentDets', (req, res) => {
-    var query = "select * from student";
-    mysql.query(query, (error, result) => {
-        if (error) throw error;
-        student = result;
-        res.render("studentDets", { student });
-    });
-})
 
+// var query = "select * from student";
+// mysql.query(query, (error, result) => {
+//     if (error) throw error;
+//     for (var i = 0; i < result.length; i++) {
+//         console.log(result[i].Stu_id);
+//     }
+// })
