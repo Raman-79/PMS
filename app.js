fc5c0485c0796efc681dd49973b515117f27c4e6
Raman-79 <ramandsat@gmail.com>
2023-01-16 22:03:57 +0530

    All tables rendered

--- app.js
+++ app.js
@@ -36,9 +36,14 @@ app.get('/adminHome', (req, res) => {
     if (admin === "admin" && password === "admin@123") {
         var query2 = "select * from company";
         var query = "select * from student";
+        var query3 = "select S.USN,C.CMP_Name,A.Status from student S ,company C ,application A where S.Stu_id = A.Stu_id and C.Cmp_id = A.Cmp_id;"
         mysql.query(query, (error, result) => {
             mysql.query(query2, (error, result2) => {
-                res.render("adminHome", { students: result, company: result2 })
+                mysql.query(query3, (error, result3) => {
+
+                    res.render("adminHome", { students: result, company: result2, applications: result3 })
+                })
+
             })
         });
 
@@ -50,12 +55,16 @@ app.get('/adminHome', (req, res) => {
 })
 app.get('/delete-student', (req, res) => {
     const id = req.query.id;
-    var address = "http://localhost:5000/adminHome?admin=admin&password=admin%40123"
+    var address = "http://localhost:" + port + "/adminHome?admin=admin&password=admin%40123"
     var query = "delete from student where Stu_id = ?";
     mysql.query(query, [id], (error, result) => {
         if (error) throw error;
         res.redirect(address);
     })
+});
+
+app.get('/update-form', (req, res) => {
+    res.render('update_form')
 })
 
 
@@ -67,9 +76,10 @@ app.get('/signup', (req, res) => {
 app.post('/signup', (req, res) => {
     var { name, usn, cgpa, batch, branch_code, password } = req.body;
     var query = "insert into student (Name,USN,Branch_code,CGPA,Batch,Password) values ('" + name + "','" + usn + "','" + branch_code + "','" + cgpa + "','" + batch + "','" + password + "')";
+    var address = "http://localhost:" + port + "/studentHome?usn=" + usn + "&password=" + password
     mysql.query(query, (error, result) => {
         if (error) throw error;
-        res.send("Registration Successfull")
+        res.redirect(address);
     })
 })
 app.get('/login', (req, res) => {
@@ -95,7 +105,7 @@ app.get('/studentHome', (req, res) => {
 
 app.get('/submit-application', (req, res) => {
     const { usn, password, CMP_id, Stu_id } = req.query;
-    var address = "http://localhost:5000/studentHome?usn=" + usn + "&password=" + password
+    var address = "http://localhost:" + port + "/studentHome?usn=" + usn + "&password=" + password
     var query = "insert into application (Cmp_id,Stu_id,Status) values ('" + CMP_id + "','" + Stu_id + "','Pending') "
     mysql.query(query, (error, result) => {
         res.redirect(address)
@@ -121,9 +131,33 @@ app.get('/cmpHome', (req, res) => {
             res.render('cmpLogin', { message: false });
         }
         else {
-            res.render('cmpHome', { result, message: true });
+            const CMP_id = result[0].CMP_Id;
+            var query1 = "select S.USN,A.Status,S.Stu_id from student as S , application as A where S.Stu_id = A.Stu_id and A.Cmp_id =" + CMP_id;
+            mysql.query(query1, (error, result1) => {
+                if (error) throw error;
+                res.render('cmpHome', { result, students: result1, message: true });
+            })
+
         }
     });
+})
+
+
+
+app.get('/change_application', (req, res) => {
+    const { Stu_id, status, CMP_Id } = req.query;
+    var data = [status, Stu_id, parseInt(CMP_Id)];
+    var query = "update application set Status = ? where Stu_id = ? and Cmp_id = ?";
+    var query2 = "select * from company where CMP_Id = ?";
+    mysql.query(query, data, (error, result) => {
+        if (error) throw error;
+        mysql.query(query2, CMP_Id, (error, result1) => {
+            if (error) throw error;
+            var address = "http://localhost:" + port + "/cmpHome?Cmpname=" + result1[0].CMP_Name + "&password=" + result1[0].Cmp_password;
+            res.redirect(address);
+        })
+
+    })
 })
 
 app.get('/cmpSignUp', (req, res) => {
@@ -131,13 +165,14 @@ app.get('/cmpSignUp', (req, res) => {
 });
 app.post('/cmpSignUp', (req, res) => {
     var { Cmpname, Role, Criteria, App_deadline, Description, Salary, Password } = req.body;
+    var address = "http://localhost:" + port + "/cmpHome?Cmpname=" + Cmpname + "&password=" + Password
     var q1 = "insert into company (Cmp_Name,Role,Criteria,App_deadline,Description,Salary,Cmp_password)  values('" + Cmpname + "','" + Role + "','" + Criteria + "','" + App_deadline + "','" + Description + "','" + Salary + "','" + Password + "') ";
     mysql.query(q1, (error, result) => {
         if (error) throw error;
-        res.send("Registered Succesfull")
+        res.redirect(address);
     });
-
 })
+
 app.listen(port, () => {
     console.log(`Server connection successfull at ${port}`);
 });
